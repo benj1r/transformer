7ac7110a68ff8cac127877fc4abab344f6f3b6b9
benj1r <47711426+benj1r@users.noreply.github.com>
2022-04-24 01:34:31 -0700

    decoder block implementation

--- decoder.py
+++ decoder.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from attention import MultiHeadAttention
+from encoder import EncoderBlock
 
 class DecoderBlock(nn.Module):
     def __init__(
@@ -11,10 +13,24 @@ class DecoderBlock(nn.Module):
             dropout,
             device
             ):
-        pass
+        super(DecoderBlock, self).__init__()
+        
+        self.embed_size = embed_size
+        self.heads = heads
+        self.fwd_expansion = fwd_expansion
+        self.dropout = dropout
+        self.device = device
+
+        self.MHA = MultiHeadAttention(self.embed_size)
+        self.norm = nn.LayerNorm(self.embed_size)
+        self.block = EncoderBlock(self.embed_size, self.heads, self.dropout, self.fwd_expansion)
+        self.dropout = nn.Dropout(self.dropout)
 
     def forward(self, x, values, keys, src_mask, trg_mask):
-        pass
+        masked_attention = self.MHA(x, x, x, trg_mask)
+        queries = self.dropout(self.norm(attention+x))
+        out = self.block(values, keys, queries, src_mask)
+        return out
 
 
 class Decoder(nn.Module):
