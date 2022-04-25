ca3973aac5251826e7de3e877c6bd4948ba4d3f1
benj1r <47711426+benj1r@users.noreply.github.com>
2022-04-24 23:03:30 -0700

    transformer implemented

--- transformer.py
+++ transformer.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from encoder import Encoder
+from decoder import Decoder
 
 class Transformer(nn.Module):
     def __init__(
@@ -16,14 +18,45 @@ class Transformer(nn.Module):
             dropout,
             device,
             max_len):
-        pass
+        self.encoder = Encoder(
+                src_vocab,
+                embed_size,
+                num_layers,
+                heads,
+                device,
+                fwd_expansion,
+                dropout,
+                max_len
+                )
+
+        self.decoder = Decoder(
+                trg_vocab,
+                embed_size,
+                num_layers,
+                heads,
+                fwd_expansion,
+                dropout,
+                device,
+                max_len
+                )
+        
+        self.src_pad = src_pad
+        self.trg_pad = trg_pad
 
     def gen_src_mask(self, src):
-        pass
+        src_mask = (src != self.src_pad).unsqueeze(1).unsqueeze(2)
+        return src_mask.to(self.device)
 
     def gen_trg_mask(self, trg):
-        pass
+        N, trg_len = trg.shape
+        mask = torch.tril(torch.ones((trg_len, trg_len))).expand(N, 1, trg_len, trg_len)
+        return mask.to(self.device)
 
     def forward(self, src, trg):
-        pass
+        src_mask = gen_src_mask(src)
+        trg_mask = gen_trg_mask(src)
+
+        enc = self.encoder(src, src_mask)
+        dec = self.decoder(trg, enc, src_mask, trg_mask)
+        return dec
 
