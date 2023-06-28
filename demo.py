7d0f7cdf19657ed33b740fb878c7bad1dca3e5b5
benj1r <47711426+benj1r@users.noreply.github.com>
2023-06-28 06:52:49 -0500

    minimal model test

--- demo.py
+++ demo.py
@@ -1,11 +1,38 @@
 import torch
-import torchtext
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
+
+from tqdm import tqdm
+from train import Trainer
+from test import Tester
+
+from transformer import Transformer
 
 def main():
-    pass
+    device = torch.device("cpu") #= torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
 
+    x = [[1,5,6,4,3,9,2,1], [2,6,7,5,4,1,3,1]].to(device)
+    y = [[2,6,7,5,4,1,3,1], [1,5,6,4,3,9,2,1]].to(device)
+
+    src_vocab = 10
+    trg_vocab = 10
+    src_pad = 0
+    trg_pad = 0
+
+    # model parameters
+    model = Transformer(
+            src_vocab=src_vocab,
+            trg_vocab=trg_vocab,
+            src_pad=src_pad,
+            trg_pad=trg_pad).to(device)
+
+    # training parameters
+
+    # optimizer
+    # optimizer = optim.Adam(model.parameters(), lr=lr)
+
+
+    out = model(x, y)
+    print(out[0])
+    
 if __name__ == "__main__":
     main()
