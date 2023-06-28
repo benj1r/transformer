9d432ad117b31f1812b51f1dd0844ad2fbcd8729
benj1r <47711426+benj1r@users.noreply.github.com>
2023-06-28 05:33:56 -0500

    demo and data files initialized

---
+++ data.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+import os.path as osp
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ demo.py
@@ -0,0 +1,11 @@
+import torch
+import torchtext
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
